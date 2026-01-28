5d4ba707b1990068f68f961e4bdb736fcc1f3764
Joan Frago <jfragojimenez@gmail.com>
2026-01-28 23:42:43 +0100

    Disparar bala

--- Eventos/Exercici_3/eventos.js
+++ Eventos/Exercici_3/eventos.js
@@ -1,7 +1,29 @@
-document.addEventListener("mousemove", (event) => {
-	console.log("Mouse position: " + event.clientX + "," + event.screenY);
+let quadrat = document.getElementById("quadrat");
 
-	let quadrat = document.getElementById("quadrat");
+document.addEventListener("mousemove", (event) => {
 	quadrat.style.left = event.clientX + 'px';
+});
+
+document.addEventListener("keypress", (event) => {
+	let bala = document.createElement("div");
+	bala.className = "bala";
+
+	bala.style.left = quadrat.style.left;
+	bala.style.bottom = "105px";
+
+	document.body.appendChild(bala);
+
+	let balaBottom = 105;
+	let intervalId = setInterval(() => {
+		bala.style.bottom = balaBottom + "px";
+		balaBottom += 2;
+
+		if (balaBottom > window.innerHeight) {
+			clearInterval(intervalId);
+			bala.remove();
+		}
+	}, 5);
+
+
 });
 
