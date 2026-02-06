56c8438187fe1442fe7e0bcac6c0c55535ba30c5
Joan Frago <jfragojimenez@gmail.com>
2026-02-06 10:44:18 +0100

    Ejercicio 3 ampliado

--- POO/Ejercicio_3/personajes.js
+++ POO/Ejercicio_3/personajes.js
@@ -2,16 +2,20 @@ class Personaje {
 	nombre;
 	nivel;
 	puntosVida;
+	arma;
 
-	constructor(nombre, nivel, puntosVida) {
+	constructor(nombre, nivel, puntosVida, arma) {
 		this.nombre = nombre;
 		this.nivel = nivel;
 		this.puntosVida = puntosVida;
+		this.arma = arma;
 	}
 
 	atacar() {
 		throw new Error("atacar: This method must be implemented by subclass.");
 	}
+
+	equiparArma(arma) { this.arma = arma; }
 
 	toString() {
 		return `Personaje:\n\tNombre -> ${this.nombre}\n\tNivel -> ${this.nivel}\n\tPuntos de Vida -> ${this.puntosVida}`;
@@ -21,8 +25,8 @@ class Personaje {
 class Guerrero extends Personaje {
 	fuerza;
 
-	constructor(nombre, nivel, puntosVida, fuerza) {
-		super(nombre, nivel, puntosVida);
+	constructor(nombre, nivel, puntosVida, arma, fuerza) {
+		super(nombre, nivel, puntosVida, arma);
 		this.fuerza = fuerza;
 	}
 
@@ -32,7 +36,7 @@ class Guerrero extends Personaje {
 
 	atacar(other) {
 		console.log(this.golpeEspada(other));
-		other.puntosVida -= this.fuerza;
+		other.puntosVida -= this.arma.atacar() * this.fuerza;
 	}
 }
 
@@ -40,8 +44,8 @@ class Mago extends Personaje {
 	hechizos;
 	fuerza;
 
-	constructor(nombre, nivel, puntosVida, hechizos, fuerza) {
-		super(nombre, nivel, puntosVida);
+	constructor(nombre, nivel, puntosVida, arma, hechizos, fuerza) {
+		super(nombre, nivel, puntosVida, arma);
 		this.hechizos = hechizos;
 		this.fuerza = fuerza;
 	}
@@ -53,8 +57,44 @@ class Mago extends Personaje {
 
 	atacar(other) {
 		console.log(this.lanzarHechizo(other));
-		other.puntosVida -= this.fuerza;
+		other.puntosVida -= this.arma.atacar() * this.fuerza;
 	}
+}
+
+class Espada {
+	nombre;
+	dano;
+
+	constructor(nombre) {
+		this.nombre = nombre;
+		this.dano = 3;
+	}
+
+	atacar() { return this.dano; }
+}
+
+class Hacha {
+	nombre;
+	dano;
+
+	constructor(nombre) {
+		this.nombre = nombre;
+		this.dano = 5;
+	}
+
+	atacar() { return this.dano; }
+}
+
+class BastonMagico {
+	nombre;
+	dano;
+
+	constructor(nombre) {
+		this.nombre = nombre;
+		this.dano = 2;
+	}
+
+	atacar() { return this.dano; }
 }
 
 function getRandomInt(max) {
@@ -62,11 +102,17 @@ function getRandomInt(max) {
 }
 
 function init() {
-	let m1 = new Mago("Mago", 8, 14, ["h1", "h2", "h3", "h4"], 4);
-	let g1 = new Guerrero("Guerrero", 5, 10, 2);
+	let a1 = new BastonMagico("BastonMagico");
+	let m1 = new Mago("Mago", 8, 14, a1, ["h1", "h2", "h3", "h4"], 0.2);
+
+	let a2 = new Hacha("Hacha");
+	let g1 = new Guerrero("Guerrero", 5, 10, a2, 0.4);
 
 	console.log("Vida de " + g1.nombre + " -> " + g1.puntosVida);
 	console.log("Vida de " + m1.nombre + " -> " + m1.puntosVida);
+
+	let a3 = new Espada("Espada");
+	g1.equiparArma(a3);
 
 	g1.atacar(m1);
 	console.log("Vida de " + m1.nombre + " -> " + m1.puntosVida);
